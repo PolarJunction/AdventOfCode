d4e250c04bc63c4528f3e8782af3fb6f5b44f222
Polar <19593942+PolarJunction@users.noreply.github.com>
2020-12-02 14:36:34 +0000

    Add DayOne Solution

--- DayOne/DayOne.cpp
+++ DayOne/DayOne.cpp
@@ -1,36 +1,49 @@
-/* Day One */
-#include <cmath>
-#include <stdio.h>
+/* Day One - 2020 */
 #include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 
-int calc_fuel (int mass)
-{
-	return (floor(mass/3)-2);
-}
+const int tgt_val = 2020;
 
 int main ()
 {
 	// Process the input file
 	std::ifstream in("input.txt");
+	std::istream_iterator<int> start(in), end;
+	std::vector<int> t_input(start, end);
 
-	int n = 0;
-	int tot_fuel = 0;
-	int fuel_req = 0;
-
-	while(in >> n)
+	// Part One
+	for (int i = 0; i < t_input.size(); i++)
 	{
-		do 
+		// Loop through the vector, subtract the value from the tgt_val and check if the result is in the array
+		auto it = std::find(t_input.begin(), t_input.end(), (tgt_val - t_input[i])); 
+
+		// If we found the result of the subtraction in the vector, use it to find the multiplication 
+		if (it != t_input.end())
 		{
-			fuel_req = calc_fuel(n);
-			
-			if (fuel_req > 0)
-			{
-				tot_fuel += fuel_req;
-				n = fuel_req;
-			}
-		} while (fuel_req > 0);
+			printf("Part One - Result Found: %u\n", (t_input[it - t_input.begin()] * t_input[i]));
+			break;
+		}
 	}
 
-	printf("Total Required Fuel: %u", tot_fuel);
-    return 0;
+	// Part Two
+	for(int i = 0; i < t_input.size(); i++)
+	{
+		// For each value in t_input, subtract each value in the vector, then check if the result is present
+		for(int j = 0; j < t_input.size(); j++)
+		{
+			auto it = std::find(t_input.begin(), t_input.end(), (tgt_val - t_input[i] - t_input[j]));
+
+			if (it != t_input.end())
+			{
+				int i_val = (t_input[it - t_input.begin()] * t_input[i] * t_input[j]);
+				printf("Part Two - Result Found: %u\n", i_val);
+
+				// Break out early
+				i = t_input.size();
+				break;
+			}
+		}
+	}
 }
