e5d18964b6897799ebed9b7e47b341f603b4fc54
PolarJunction <19593942+PolarJunction@users.noreply.github.com>
2021-12-13 22:02:01 +0000

    Rework for CMake

--- 2021/Day-01/Day_01.cpp
+++ 2021/Day-01/Day_01.cpp
@@ -1,6 +1,6 @@
 /* Day One - 2021 */
 #define CATCH_CONFIG_MAIN
-#include "../lib/catch.hpp"
+#include <catch2/catch_test_macros.hpp>
 
 #include <fstream>
 #include <iterator>
@@ -47,24 +47,24 @@ int solvePartTwo(std::vector<int> &data)
 // Tests
 TEST_CASE("Part One - Test Data")
 {
-    std::vector<int> data = parseInput("test-01.txt");
+    std::vector<int> data = parseInput("../test-01.txt");
     REQUIRE(solvePartOne(data) == 7);
 }
 
 TEST_CASE("Part One - Input Data")
 {
-    std::vector<int> data = parseInput("input.txt");
+    std::vector<int> data = parseInput("../input.txt");
     REQUIRE(solvePartOne(data) == 1502);
 }
 
 TEST_CASE("Part Two - Test Data")
 {
-    std::vector<int> data = parseInput("test-01.txt");
+    std::vector<int> data = parseInput("../test-01.txt");
     REQUIRE(solvePartTwo(data) == 5);
 }
 
 TEST_CASE("Part Two - Input Data")
 {
-    std::vector<int> data = parseInput("input.txt");
+    std::vector<int> data = parseInput("../input.txt");
     REQUIRE(solvePartTwo(data) == 1538);
 }
